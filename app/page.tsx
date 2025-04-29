0489ef2378e2643990a536f8bc75e805040b6248
Gsoaresnori <gabrielzinho353@gmail.com>
2025-04-29 16:19:44 -0300

    feat: add Header component and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,34 @@
-"use client";
-
-import { useState } from "react";
-import { Button } from "./_components/ui/button";
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
+import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
 const Home = () => {
-  const [] = useState(); 
-  return <Button>text</Button>;
+  return (
+    <div>
+      {/* header */}
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Gabriel</h2>
+        <p>Segunda-feira, 05 de Agosto.</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+        <div className="relative mt-6 h-[150px] w-full rounded-xl">
+          <Image
+            src="/banner-01.png"
+            fill
+            className="object-cover"
+            alt="Agende nos melhores com NavalhaHub"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
-export default Home;
+export default Home
