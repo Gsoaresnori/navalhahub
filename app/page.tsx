451a568c1eb245722123f6a3a98c960593bc0b0f
Gsoaresnori <gabrielzinho353@gmail.com>
2025-04-29 17:50:37 -0300

    feat: add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
+import { Badge } from "./_components/ui/badge"
 
 const Home = () => {
   return (
@@ -10,16 +13,19 @@ const Home = () => {
       {/* header */}
       <Header />
       <div className="p-5">
+        {/* Texto */}
         <h2 className="text-xl font-bold">Olá, Gabriel</h2>
         <p>Segunda-feira, 05 de Agosto.</p>
 
         <div className="mt-6 flex items-center gap-2">
+          {/* Busca */}
           <Input placeholder="Faça sua busca..." />
           <Button>
             <SearchIcon />
           </Button>
         </div>
         <div className="relative mt-6 h-[150px] w-full rounded-xl">
+          {/* Banner */}
           <Image
             src="/banner-01.png"
             fill
@@ -27,6 +33,29 @@ const Home = () => {
             alt="Agende nos melhores com NavalhaHub"
           />
         </div>
+        {/* Agendamento */}
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            {/* Esquerda */}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+              <div className="flex items-center">
+                <Avatar className="h6 w-6">
+                  <AvatarImage src="https://github.com/shadcn.png" />
+                </Avatar>
+                <p className="text-sm">Corte de Cabelo</p>
+              </div>
+            </div>
+
+            {/* Direita */}
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Fevereiro</p>
+              <p className="text-2xl">06</p>
+              <p className="text-sm">09:45</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
