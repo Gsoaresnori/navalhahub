8ff16a4c9f63ff4a91c23afd2a236c866ac0621d
Gsoaresnori <gabrielzinho353@gmail.com>
2025-05-01 19:16:26 -0300

    chore: add next auth setup

--- app/_components/ui/sidebar-sheet.tsx
+++ app/_components/ui/sidebar-sheet.tsx
@@ -1,10 +1,18 @@
 import Image from "next/image"
 import { Button } from "./button"
-import { CalendarIcon, HomeIcon, LogOutIcon } from "lucide-react"
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react"
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./sheet"
 import Link from "next/link"
 import { quickSearchOptions } from "@/app/_constants/search"
 import { Avatar, AvatarImage } from "../ui/avatar"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog"
 
 const SidebarSheet = () => {
   return (
@@ -12,6 +20,33 @@ const SidebarSheet = () => {
       <SheetHeader>
         <SheetTitle className="text-left">Menu</SheetTitle>
       </SheetHeader>
+      <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
+        <h2 className="text-lg font-bold">Olá, faça seu login!</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="icon">
+              <LogInIcon />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[90%] rounded-xl">
+            <DialogHeader>
+              <DialogTitle>Faça login na plataforma!</DialogTitle>
+              <DialogDescription>
+                Conecte-se usando sua conta do Google
+              </DialogDescription>
+            </DialogHeader>
+            <Button variant="outline" className="gap-1 font-bold">
+              <Image
+                src="/google.svg"
+                alt="Fazer login com o Google"
+                width={18}
+                height={18}
+              />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+      </div>
       <div className="flex items-center gap-3 border-b border-solid py-5">
         <Avatar>
           <AvatarImage src="https://utfs.io/f/e995db6d-df96-4658-99f5-11132fd931e1-17j.png" />
